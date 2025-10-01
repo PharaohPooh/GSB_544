0d0499ba51676843e8383ec901c5d7322c617a76
PharaohPooh <jamescompagno@yahoo.com>
2025-10-01 15:15:24 -0700

    Practice activity

--- Week_2/Practice_Activity_2.1_Data_Wrangling/Wednesday10_1_25.py
+++ Week_2/Practice_Activity_2.1_Data_Wrangling/Wednesday10_1_25.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+# Population data from GapMinder
+population = pd.read_csv("pop.csv")
+
+
+#Wide to Long
+long_population = population.melt(id_vars=["country"], var_name="year", value_name="population")
+long_population.head
+
+#Long to wide
+wide_population = long_population.pivot(index = "country", columns = "year", values = "population")
+wide_population = wide_population.reset_index()
+wide_population.head
